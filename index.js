44e66848b06fddfc29e583606e9534fd5f763069
Mohammed Mahdi <mak.zero.x@gmail.com>
2017-10-08 23:35:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+
+function happyHolidays() {
+  var happyHolidays == `Happy holidays!`
+  returns happyHolidays
+}
+
+
+/*function happyHolidaysTo(name){
+returns `Happy holidays, ${name}!`
+}
+
+happyHolidays()
+happyHolidaysTo("you")
+
+function say(greeting, firstName) {
+  console.log('I was called!')
+  return `${greeting}, ${firstName}!`
+}
